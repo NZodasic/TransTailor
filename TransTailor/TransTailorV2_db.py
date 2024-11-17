802946008016b66a9318fab6100e0c6b68548d3f
Nguyễn Võ Chí Dũng <bbiacm007@gmail.com>
2024-11-17 17:30:27 +0700

    feat: Normalize and testing

--- TransTailor/TransTailorV2_db.py
+++ TransTailor/TransTailorV2_db.py
@@ -10,18 +10,20 @@ import time
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
-TA_EPOCH = 40
-TA_LR = 0.00005
+TA_EPOCH = 1
+TA_LR = 0.005
 TA_MOMENTUM = 0.9
 
 IA_EPOCH = 1
 IA_LR = 0.005
 IA_MOMENTUM = 0.9
 
+PRUNING_AMOUNT = 5
+
 def LoadModel(device):
     # Load the VGG16 model
     model = torchvision.models.vgg16(weights=torchvision.models.VGG16_Weights.IMAGENET1K_V1)
-   
+
     # Replace the last layer of the model with a new layer that matches the number of classes in CIFAR10
     num_classes = 10
     model.classifier[6] = torch.nn.Linear(model.classifier[6].in_features, num_classes)
@@ -31,53 +33,24 @@ def LoadModel(device):
     return model
 
 def LoadData(numWorker, batchSize):
-    # # Define the data transformation
-    # transform = transforms.Compose([
-    #     transforms.RandomResize(224),
-    #     transforms.ToTensor(),
-    #     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-
-    # transform = transforms.Compose([
-    #     transforms.RandomResizedCrop(224),
-    #     transforms.RandomHorizontalFlip(),
-    #     transforms.RandomRotation(15),
-    #     transforms.ColorJitter(brightness=0.2, contrast=0.2),
-    #     transforms.ToTensor(),
-    #     transforms.Normalize(mean=[0.485, 0.456, 0.406], 
-    #                         std=[0.229, 0.224, 0.225])
-    # ])
-    
+    # Define the data transformation
     transform = transforms.Compose([
         transforms.Resize((224, 224)),
-        transforms.RandomHorizontalFlip(p=0.5),
-        transforms.RandomRotation(15),
-        transforms.ColorJitter(brightness=0.2, contrast=0.2, saturation=0.2),
-        transforms.RandomAffine(degrees=0, translate=(0.1, 0.1)),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], 
-                           std=[0.229, 0.224, 0.225]),
-        transforms.RandomErasing(p=0.2)
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
 
-    
     data_path = os.path.join(ROOT_DIR, "data")
 
     # Load the CIFAR10 train_dataset
-    train_dataset = torchvision.datasets.CIFAR10(root=data_path, 
-                                                 train=True, 
-                                                 download=True, 
-                                                 transform=transform)
+    train_dataset = torchvision.datasets.CIFAR10(root=data_path, train=True, download=True, transform=transform)
 
     kwargs = {'num_workers': numWorker, 'pin_memory': True} if device == 'cuda' else {}
     train_loader = torch.utils.data.DataLoader(train_dataset, batchSize, shuffle=True, **kwargs)
 
     # Load test_dataset
-    test_dataset = torchvision.datasets.CIFAR10(root=data_path, 
-                                                train=True, 
-                                                download=True, 
-                                                transform=transform)
+    test_dataset = torchvision.datasets.CIFAR10(root=data_path, train=False, download=True, transform=transform)
 
-    kwargs = {'num_workers': 4, 'pin_memory': True} if device == 'cuda' else {}
     test_loader = torch.utils.data.DataLoader(test_dataset, batchSize, shuffle=False, **kwargs)
 
     return train_loader, test_loader
@@ -97,19 +70,20 @@ def LoadArguments():
 
     return ROOT_DIR, CHECKPOINT_PATH, NUM_WORKER, BATCH_SIZE
 
-def CalculateAccuracy(model, test_loader):
+def CalculateAccuracy(model, test_loader, device):
     model.eval()
-    total_correct = 0
-    total_samples = 0
+    correct = 0
+    total = 0
+    
     with torch.no_grad():
-        for images, labels in test_loader:
-            images, labels = images.to(device), labels.to(device)
-            outputs = model(images)
+        for inputs, labels in test_loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
             _, predicted = torch.max(outputs.data, 1)
-            total_correct += (predicted == labels).sum().item()
-            total_samples += len(labels)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
 
-    accuracy = 100 * total_correct / total_samples
+    accuracy = 100 * correct / total
     return accuracy
 
 def TimeLog():
@@ -121,71 +95,89 @@ if __name__ == "__main__":
     # LOAD ARGUMENTS
     logger.info("START MAIN PROGRAM!")
     ROOT_DIR, CHECKPOINT_PATH, NUM_WORKER, BATCH_SIZE = LoadArguments()
-    RESULT_PATH = os.path.join(ROOT_DIR, "checkpoint/optimal_model.pt")
-    SAVED_PATH = os.path.join(ROOT_DIR, "checkpoint/pruner/checkpoint_{pruned_count}.pkl")
+    
+    RESULT_PATH = os.path.join(CHECKPOINT_PATH, "optimal_model.pt")
+    SAVED_PATH = os.path.join(CHECKPOINT_PATH, "pruner", "checkpoint_{pruned_count}.pkl")
 
     # LOAD MODEL
     logger.info("GET DEVICE INFORMATION")
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     logger.info("DEVICE: " + str(device))
 
+    # LOAD DATASET
     logger.info("LOAD DATASET: CIFAR10")
     train_loader, test_loader = LoadData(NUM_WORKER, BATCH_SIZE)
 
     logger.info("LOAD PRETRAINED MODEL: VGG-16 (ImageNet)")
     model = LoadModel(device)
 
-    # INIT PRUNING SCHEME
-    pruner = Pruner(model, train_loader, device, amount=10)
+    # # INIT PRUNING SCHEME
+    pruner = Pruner(model, train_loader, device)
     if os.path.isfile(CHECKPOINT_PATH):
         logger.info("Load model and pruning info from checkpoint...")
         pruner.LoadState(CHECKPOINT_PATH)
     else:
-        pruner.Finetune(40, TA_LR, TA_MOMENTUM, 5)
+        pruner.Finetune(40, TA_LR, TA_MOMENTUM, 0)
 
         pruner.InitScalingFactors()
         pruner.SaveState(SAVED_PATH.format(pruned_count = 0))
 
     opt_accuracy = CalculateAccuracy(pruner.model, test_loader)
     print("Accuracy of finetuned model: ", opt_accuracy, flush=True)
+    logger.info("Accuracy of finetuned model: ", opt_accuracy, flush=True)
+
 
     # START PRUNING PROCESS
     while True:
         TimeLog()
+        pruner.TrainScalingFactors(ROOT_DIR, IA_EPOCH, IA_LR, IA_MOMENTUM)
         
-        pruner.TrainScalingFactors(ROOT_DIR, 1, IA_LR, IA_MOMENTUM)
         TimeLog()
-        
         pruner.GenerateImportanceScores()
         
-        filters_to_prune = pruner.FindFiltersToPrune()
-        TimeLog()
+        # layer_to_prune, filter_to_prune = pruner.FindFilterToPrune()
+        filters_to_prune = pruner.FindFiltersToPrune(PRUNING_AMOUNT)
+        # pruner.Prune(layer_to_prune, filter_to_prune)
         
+        TimeLog()
         pruner.PruneAndRestructure(filters_to_prune)
-        TimeLog()
         
+        TimeLog()
         pruner.ModifyClassifier()
-        TimeLog()
         
+        TimeLog()
         pruner.PruneScalingFactors(filters_to_prune)
-        TimeLog()
         
+        TimeLog()
         pruner.PruneImportanceScore(filters_to_prune)
+        
+        TimeLog()
+        pruner.ImportanceAwareFineTuning(IA_EPOCH, IA_LR, IA_MOMENTUM)
         
         sum_filters = 0 
         for layer in filters_to_prune:
             number_of_filters = len(filters_to_prune[layer])
             sum_filters += number_of_filters
         print("===Number of pruned filters is: ", sum_filters)
+
+        pruned_count = len(pruner.pruned_filters)
+        if pruned_count % 10 == 0:
+            pruner.SaveState(SAVED_PATH.format(pruned_count = pruned_count))
         
-        for param in pruner.model.parameters():
-            param.requires_grad = True
+        
+
 
         TimeLog()
         pruner.Finetune(TA_EPOCH, TA_LR, TA_MOMENTUM, 0)
+        
         TimeLog()
         pruned_accuracy = CalculateAccuracy(pruner.model, test_loader)
-        print("Accuracy of pruned model: ", pruned_accuracy, flush=True)
+        
+        print("Accuracy of pruned model: {pruned_accuracy:.2f}%", flush=True)
+        logger.info(f"Accuracy of pruned model: {pruned_accuracy:.2f}%", flush=True)
+        
+        pruned_filters = len(pruner.pruned_filters)
+        print("Number of pruned filters: ", pruned_filters, flush=True)
 
         if abs(opt_accuracy - pruned_accuracy) > pruner.amount:
             print("Optimization done!", flush=True)
